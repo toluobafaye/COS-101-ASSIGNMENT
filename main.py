40c6f8cfbea306c5f41d296e5fa45cdbddbd356e
tolulope obafaye <tolu4faye@gmail.com>
2023-11-08 14:32:41 +0100

    My class project 1

---
+++ main.py
@@ -0,0 +1,161 @@
+# Function to calculate Simple Interest
+def calculate_simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+
+# Function to calculate Compound Interest
+def calculate_compound_interest(principal, rate, time, n):
+    amount = principal * (1 + (rate / (n * 100)))**(n * time)
+    interest = amount - principal
+    return interest
+
+# Input from the user
+principal = float(input("Enter the initial principal amount: $"))
+rate = float(input("Enter the annual interest rate (%): "))
+time = float(input("Enter the number of years: "))
+n = int(input("Enter the number of times interest is applied per time period: "))
+
+# Calculate Simple Interest
+simple_interest = calculate_simple_interest(principal, rate, time)
+
+# Calculate Compound Interest
+compound_interest = calculate_compound_interest(principal, rate, time, n)
+
+# Print the name of the business and the results
+print("Business Name: Yusuf and Sons")
+print(f"Simple Interest: ${simple_interest:.2f}")
+print(f"Compound Interest: ${compound_interest:.2f}")
+# Function to calculate Simple Interest
+def calculate_simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+
+# Function to calculate Compound Interest
+def calculate_compound_interest(principal, rate, time, n):
+    amount = principal * (1 + (rate / (n * 100)))**(n * time)
+    interest = amount - principal
+    return interest
+
+# Input from the user
+principal = float(input("Enter the initial principal amount: $"))
+rate = float(input("Enter the annual interest rate (%): "))
+time = float(input("Enter the number of years: "))
+n = int(input("Enter the number of times interest is applied per time period: "))
+
+# Calculate Simple Interest
+simple_interest = calculate_simple_interest(principal, rate, time)
+
+# Calculate Compound Interest
+compound_interest = calculate_compound_interest(principal, rate, time, n)
+
+# Print the name of the business and the results
+print("Business Name: Yusuf and Sons")
+print(f"Simple Interest: ${simple_interest:.2f}")
+print(f"Compound Interest: ${compound_interest:.2f}")
+# Function to calculate Simple Interest
+def calculate_simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+
+# Function to calculate Compound Interest
+def calculate_compound_interest(principal, rate, time, n):
+    amount = principal * (1 + (rate / (n * 100)))**(n * time)
+    interest = amount - principal
+    return interest
+
+# Input from the user
+principal = float(input("Enter the initial principal amount: $"))
+rate = float(input("Enter the annual interest rate (%): "))
+time = float(input("Enter the number of years: "))
+n = int(input("Enter the number of times interest is applied per time period: "))
+
+# Calculate Simple Interest
+simple_interest = calculate_simple_interest(principal, rate, time)
+
+# Calculate Compound Interest
+compound_interest = calculate_compound_interest(principal, rate, time, n)
+
+# Print the name of the business and the results
+print("Business Name: Yusuf and Sons")
+print(f"Simple Interest: ${simple_interest:.2f}")
+print(f"Compound Interest: ${compound_interest:.2f}")
+# Function to calculate Simple Interest
+def calculate_simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+
+# Function to calculate Compound Interest
+def calculate_compound_interest(principal, rate, time, n):
+    amount = principal * (1 + (rate / (n * 100)))**(n * time)
+    interest = amount - principal
+    return interest
+
+# Input from the user
+principal = float(input("Enter the initial principal amount: $"))
+rate = float(input("Enter the annual interest rate (%): "))
+time = float(input("Enter the number of years: "))
+n = int(input("Enter the number of times interest is applied per time period: "))
+
+# Calculate Simple Interest
+simple_interest = calculate_simple_interest(principal, rate, time)
+
+# Calculate Compound Interest
+compound_interest = calculate_compound_interest(principal, rate, time, n)
+
+# Print the name of the business and the results
+print("Business Name: Yusuf and Sons")
+print(f"Simple Interest: ${simple_interest:.2f}")
+print(f"Compound Interest: ${comp
+def calculate_simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+
+# Function to calculate Compound Interest
+def calculate_compound_interest(principal, rate, time, n):
+    amount = principal * (1 + (rate / (n * 100)))**(n * time)
+    interest = amount - principal
+    return interest
+
+# Input from the user
+principal = float(input("Enter the initial principal amount: $"))
+rate = float(input("Enter the annual interest rate (%): "))
+time = float(input("Enter the number of years: "))
+n = int(input("Enter the number of times interest is applied per time period: "))
+
+# Calculate Simple Interest
+simple_interest = calculate_simple_interest(principal, rate, time)
+
+# Calculate Compound Interest
+compound_interest = calculate_compound_interest(principal, rate, time, n)
+
+# Print the name of the business and the results
+print("Business Name: Yusuf and Sons")
+print(f"Simple Interest: ${simple_interest:.2f}")
+print(f"Compound Interest: ${compound_interest:.2f}")
+# Function to calculate Simple Interest
+def calculate_simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+
+# Function to calculate Compound Interest
+def calculate_compound_interest(principal, rate, time, n):
+    amount = principal * (1 + (rate / (n * 100)))**(n * time)
+    interest = amount - principal
+    return interest
+
+# Input from the user
+principal = float(input("Enter the initial principal amount: $"))
+rate = float(input("Enter the annual interest rate (%): "))
+time = float(input("Enter the number of years: "))
+n = int(input("Enter the number of times interest is applied per time period: "))
+
+# Calculate Simple Interest
+simple_interest = calculate_simple_interest(principal, rate, time)
+
+# Calculate Compound Interest
+compound_interest = calculate_compound_interest(principal, rate, time, n)
+
+# Print the name of the business and the results
+print("Business Name: Yusuf and Sons")
+print(f"Simple Interest: ${simple_interest:.2f}")
+print(f"Compound Interest: ${compound_interest:.2f}")
